bb3f12f10da6b3c601de26df337177ad0ee237e9
Tanko Fauzi <tankofauzy495@gmail.com>
2026-04-18 12:53:26 +0000

    Prepare frontend for Netlify deployment

--- jersey_react/vite.config.js
+++ jersey_react/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,11 +9,13 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-      },
-      '/uploads': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1000
   }
 })
